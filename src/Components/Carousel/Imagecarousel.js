37b12605fe566aeb149cb3a127eb505d981ba59f
Dwaylan <Dwaylan.Applewhite@gmail.com>
2021-05-13 20:24:10 -0400

    added carousel functionality

--- src/Components/Carousel/Imagecarousel.js
+++ src/Components/Carousel/Imagecarousel.js
@@ -1,18 +1,18 @@
 import React, { Component } from "react";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import "./imagecarousel.css";
 import { Carousel } from "react-responsive-carousel";
 
 export default class ImageCarousel extends Component {
   render() {
     return (
-      <Carousel>
+      <Carousel className="Carousel">
         <div>
-          <p>slide 1</p>
+          <img src="../Images/eschscholtzie.jpeg" alt="flower1"></img>
+          <p className="legend">Welcome to Eden</p>
         </div>
         <div>
-          <p>slide 2</p>
-        </div>
-        <div>
-          <p>slide 3</p>
+          <img src="../Images/goldenrod.jpeg" alt="flower2"></img>
         </div>
       </Carousel>
     );
--- src/Components/Landingpage/Landingpage.js
+++ src/Components/Landingpage/Landingpage.js
@@ -6,10 +6,7 @@ import ImageCarousel from "../Carousel/Imagecarousel";
 export default function Landingpage() {
   return (
     <div className="container">
-      <div className="Welcome_statement">
-        <h2>Welcome to the eden application</h2>
-        <h4>Designed and created by Dwaylan A. Applewhite</h4>
-      </div>
+      <div className="Welcome_statement"></div>
       <ImageCarousel></ImageCarousel>
     </div>
   );
