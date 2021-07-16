ff5ae77a400d9bd35113a9ae2ffdd7d2a2edc15a
Dwaylan <Dwaylan.Applewhite@gmail.com>
2021-07-16 12:48:56 -0400

    added footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Nav from "./Components/Nav";
 import Router from "./Router/Router";
+import Footer from "./Components/Footer";
 import { BrowserRouter } from "react-router-dom";
 import "./App.css";
 
@@ -9,6 +10,7 @@ function App() {
     <BrowserRouter>
       <Nav />
       <Router />
+      <Footer />
     </BrowserRouter>
   );
 }
---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div>
+      <h2>Dwaylan Applewhite</h2>
+    </div>
+  );
+}
